bddf46b881c0423768321c88418867be56ee47e8
SebvstiannMedina <seba.medina@duocuc.cl>
2024-10-19 04:09:14 -0300

    Productos cargados con carrito
    No pude anexar pero se ve que se puede hacer uso de native storage para eso

--- automotora/src/app/pages/aeromatizantes/aeromatizantes.page.ts
+++ automotora/src/app/pages/aeromatizantes/aeromatizantes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; // Importa SQLite y SQLiteObject
 
 @Component({
   selector: 'app-aeromatizantes',
@@ -6,40 +8,112 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aeromatizantes.page.scss'],
 })
 export class AeromatizantesPage implements OnInit {
-
   // Definir el array de productos
-  aeromatizantes = [
-    {
-      name: 'Aromatizante Glade Auto Esencia Auto Nuevo',
-      price: 12000,
-      description: 'Aeromatizante Glade.',
-      image: 'https://easycl.vtexassets.com/arquivos/ids/300411/1152016-03.jpg?v=637530216465300000'
-    },
-    {
-      name: 'Little Trees Pino Aromatizante Royal Pine 1 Unid',
-      price: 200000,
-      description: 'Aeromatizante Little Trees Pino',
-      image: 'https://santaisabel.vtexassets.com/arquivos/ids/177985/Aromatizante-para-Auto-Little-Trees-Pino-1-Unidad.jpg?v=637635341246600000'
-    },
-    {
-      name: 'Ambientador Auto Frutos Rojos 8 ml',
-      price: 5800,
-      description: 'Ambientador Auto Frutos Rojos 8 ml',
-      image: 'https://cdnx.jumpseller.com/jardindehadas/image/11817055/Ambientador-Auto-Frutos-Rojos-8-ml-Boles-d-olor-8432097083734.jpg?1603486397'
-    },
-    {
-      name: 'Pack 3 Aromatizantes Auto Deosol Variedad De Aromas',
-      price: 10.990,
-      description: 'Aeromatizantes tree pack, variedades',
-      image: 'https://http2.mlstatic.com/D_NQ_NP_900664-MLC52802648376_122022-O.webp'
-    },
-    // Añadir más productos aquí
-  ];
+  products: any[] = [];
+  cart: any[] = [];
 
-  constructor() { }
+  // Base de datos SQLite
+  database: SQLiteObject | null = null; // Inicializar como null
+
+  constructor(
+    private navCtrl: NavController,
+    private sqlite: SQLite // Inyecta el servicio SQLite
+  ) {}
 
   ngOnInit() {
-    // Inicialización si es necesario
+    // Inicializar la base de datos
+    this.sqlite.create({
+      name: 'automotora.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      this.database = db;
+      console.log('Base de datos creada con éxito');
+      this.createTable(); // Crear tabla si no existe
+      this.insertAeromatizantes(); // Insertar productos si la tabla está vacía
+      this.selectAeromatizantes(); // Cargar productos
+    }).catch(e => {
+      console.log('Error al crear la base de datos', e);
+    });
   }
 
+  // Crear tabla si no existe
+  createTable() {
+    if (this.database) {
+      this.database.executeSql(`
+        CREATE TABLE IF NOT EXISTS aeromatizantes (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nombre TEXT,
+          descripcion TEXT,
+          imagen TEXT,
+          precio INTEGER
+        );`, [])
+        .then(() => {
+          console.log('Tabla "aeromatizantes" creada o ya existe');
+        })
+        .catch(e => {
+          console.log('Error al crear la tabla', e);
+        });
+    }
+  }
+
+  // Insertar productos en la tabla
+  insertAeromatizantes() {
+    if (this.database) {
+      this.database.executeSql(`
+        INSERT OR IGNORE INTO aeromatizantes (nombre, descripcion, imagen, precio) VALUES 
+        ('Aromatizante Glade Auto Esencia Auto Nuevo', 'Aeromatizante Glade.', 'https://easycl.vtexassets.com/arquivos/ids/300411/1152016-03.jpg?v=637530216465300000', 12000),
+        ('Little Trees Pino Aromatizante Royal Pine 1 Unid', 'Aeromatizante Little Trees Pino', 'https://santaisabel.vtexassets.com/arquivos/ids/177985/Aromatizante-para-Auto-Little-Trees-Pino-1-Unidad.jpg?v=637635341246600000', 200000),
+        ('Ambientador Auto Frutos Rojos 8 ml', 'Ambientador Auto Frutos Rojos 8 ml', 'https://cdnx.jumpseller.com/jardindehadas/image/11817055/Ambientador-Auto-Frutos-Rojos-8-ml-Boles-d-olor-8432097083734.jpg?1603486397', 5800),
+        ('Pack 3 Aromatizantes Auto Deosol Variedad De Aromas', 'Aeromatizantes tree pack, variedades', 'https://http2.mlstatic.com/D_NQ_NP_900664-MLC52802648376_122022-O.webp', 10990);
+      `, [])
+        .then(() => {
+          console.log('Aeromatizantes insertados correctamente');
+          this.selectAeromatizantes(); // Refrescar listado de productos
+        })
+        .catch(e => console.log('Error al insertar aeromatizantes', e));
+    }
+  }
+
+  // Seleccionar productos desde la base de datos
+  selectAeromatizantes() {
+    if (this.database) {
+      this.database.executeSql('SELECT * FROM aeromatizantes', []).then(res => {
+        this.products = [];
+        for (let i = 0; i < res.rows.length; i++) {
+          this.products.push(res.rows.item(i));
+        }
+        console.log('Productos cargados:', this.products);
+      })
+      .catch(e => {
+        console.log('Error al cargar productos', e);
+      });
+    }
+  }
+
+  // Añadir producto al carrito
+  addToCart(product: any) {
+    const existingProduct = this.cart.find(item => item.nombre === product.nombre);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      this.cart.push({ ...product, quantity: 1 });
+    }
+  }
+
+  // Eliminar producto del carrito
+  removeFromCart(product: any) {
+    this.cart = this.cart.filter(item => item.nombre !== product.nombre);
+  }
+
+  // Vaciar el carrito
+  clearCart() {
+    this.cart = [];
+  }
+
+  // Navegar al carrito de compras
+  goToCart() {
+    this.navCtrl.navigateForward('/carrito-compra', {
+      state: { cart: this.cart }
+    });
+  }
 }
--- automotora/src/app/pages/otros/otros.page.ts
+++ automotora/src/app/pages/otros/otros.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; // Importa SQLite y SQLiteObject
 
 @Component({
   selector: 'app-otros',
@@ -6,41 +8,112 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./otros.page.scss'],
 })
 export class OtrosPage implements OnInit {
-
- 
   // Definir el array de productos
-  products = [
-    {
-      name: ' Espejo Retrovisor ',
-      price: 24990,
-      description: 'Espejo Retrovisor universal convexo ',
-      image: 'https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/127181553_01/w=800,h=800,fit=pad'
-    },
-    {
-      name: 'Led intermitente',
-      price: 7664,
-      description: 'X2 Ampolleta 144 Led Intermitente Py21 Pata En V 12v Ámbar',
-      image: 'https://http2.mlstatic.com/D_NQ_NP_758773-MLC53781392571_022023-O.webp'
-    },
-    {
-      name: 'Funda para asientos',
-      price: 55000,
-      description: 'Funda Cubre Asientos para Autos Negro con Tonos Grices MOMO',
-      image: 'https://api.autoplanet.cl/medias/sys_master/images/hc9/h68/9633207287838/1054054_1-1682041641/1054054-1-1682041641.webp'
-    },
-    {
-      name: 'Palanca de cambio',
-      price: 28310,
-      description: 'Articulacion Palanca Cambio Chevrolet Corsa 1.4 1.6 94 - 12',
-      image: 'https://http2.mlstatic.com/D_NQ_NP_2X_607758-MLC73371013918_122023-F.webp'
-    },
-    // Añadir más productos aquí
-  ];
+  products: any[] = [];
+  cart: any[] = [];
 
-  constructor() { }
+  // Base de datos SQLite
+  database: SQLiteObject | null = null;  // Inicializar como null
+
+  constructor(
+    private navCtrl: NavController, 
+    private sqlite: SQLite // Inyecta el servicio SQLite
+  ) {}
 
   ngOnInit() {
-    // Inicialización si es necesario
+    // Inicializar la base de datos
+    this.sqlite.create({
+      name: 'automotora.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      this.database = db;
+      console.log('Base de datos creada con éxito');
+      this.createTable(); // Crear tabla si no existe
+      this.insertOtros(); // Insertar productos si la tabla está vacía
+      this.seleccionarCrud(); // Cargar productos
+    }).catch(e => {
+      console.log('Error al crear la base de datos', e);
+    });
   }
 
+  // Crear tabla si no existe
+  createTable() {
+    if (this.database) {
+      this.database.executeSql(`
+        CREATE TABLE IF NOT EXISTS otros (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nombre TEXT,
+          descripcion TEXT,
+          imagen TEXT,
+          precio INTEGER
+        );`, [])
+        .then(() => {
+          console.log('Tabla "otros" creada o ya existe');
+        })
+        .catch(e => {
+          console.log('Error al crear la tabla', e);
+        });
+    }
+  }
+
+  // Insertar productos en la tabla
+  insertOtros() {
+    if (this.database) {
+      this.database.executeSql(`
+        INSERT OR IGNORE INTO otros (nombre, descripcion, imagen, precio) VALUES 
+        ('Espejo Retrovisor', 'Espejo Retrovisor universal convexo', 'https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/127181553_01/w=800,h=800,fit=pad', 24990),
+        ('Led intermitente', 'X2 Ampolleta 144 Led Intermitente Py21 Pata En V 12v Ámbar', 'https://http2.mlstatic.com/D_NQ_NP_758773-MLC53781392571_022023-O.webp', 7664),
+        ('Funda para asientos', 'Funda Cubre Asientos para Autos Negro con Tonos Grices MOMO', 'https://api.autoplanet.cl/medias/sys_master/images/hc9/h68/9633207287838/1054054_1-1682041641/1054054-1-1682041641.webp', 55000),
+        ('Palanca de cambio', 'Articulacion Palanca Cambio Chevrolet Corsa 1.4 1.6 94 - 12', 'https://http2.mlstatic.com/D_NQ_NP_2X_607758-MLC73371013918_122023-F.webp', 28310);
+      `, [])
+        .then(() => {
+          console.log('Productos insertados correctamente');
+          this.seleccionarCrud(); // Refrescar listado de productos
+        })
+        .catch(e => console.log('Error al insertar productos', e));
+    }
+  }
+
+  // Seleccionar productos desde la base de datos
+  seleccionarCrud() {
+    if (this.database) {
+      this.database.executeSql('SELECT * FROM otros', []).then(res => {
+        this.products = [];
+        for (let i = 0; i < res.rows.length; i++) {
+          this.products.push(res.rows.item(i));
+        }
+        console.log('Productos cargados:', this.products);
+      })
+      .catch(e => {
+        console.log('Error al cargar productos', e);
+      });
+    }
+  }
+
+  // Añadir producto al carrito
+  addToCart(product: any) {
+    const existingProduct = this.cart.find(item => item.nombre === product.nombre);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      this.cart.push({ ...product, quantity: 1 });
+    }
+  }
+
+  // Eliminar producto del carrito
+  removeFromCart(product: any) {
+    this.cart = this.cart.filter(item => item.nombre !== product.nombre);
+  }
+
+  // Vaciar el carrito
+  clearCart() {
+    this.cart = [];
+  }
+
+  // Navegar al carrito de compras
+  goToCart() {
+    this.navCtrl.navigateForward('/carrito-compra', {
+      state: { cart: this.cart }
+    });
+  }
 }
