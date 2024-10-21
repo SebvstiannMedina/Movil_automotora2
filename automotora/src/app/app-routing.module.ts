b0536bb9389b18c8ef99d4165d5db7138dd2848d
SebvstiannMedina <seba.medina@duocuc.cl>
2024-10-20 23:00:59 -0300

    a

--- automotora/src/app/app-routing.module.ts
+++ automotora/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home', //lo cambie solo para que lo veas
+    redirectTo: 'registro-venta', //lo cambie solo para que lo veas
     pathMatch: 'full'
   },
   {
