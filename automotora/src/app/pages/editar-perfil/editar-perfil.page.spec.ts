123b57860642fd9282f7a76c5d5f404c4b9bd374
Angel Rengifo <angelrengifo140@gmail.com>
2024-11-12 01:45:40 -0300

    2/05
    pido perdon en la tarde quede dormido xdddd

--- automotora/src/app/pages/editar-perfil/editar-perfil.page.spec.ts
+++ automotora/src/app/pages/editar-perfil/editar-perfil.page.spec.ts
@@ -1,17 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { EditarPerfilPage } from './editar-perfil.page';
+import { IonicModule, AlertController } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ServiceBDService } from 'src/app/service/service-bd.service';
+import { of } from 'rxjs';
 
 describe('EditarPerfilPage', () => {
   let component: EditarPerfilPage;
   let fixture: ComponentFixture<EditarPerfilPage>;
+  let alertController: AlertController;
+  let serviceBDService: jasmine.SpyObj<ServiceBDService>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    const serviceSpy = jasmine.createSpyObj('ServiceBDService', ['modificarUsuario']);
+
+    TestBed.configureTestingModule({
+      declarations: [EditarPerfilPage],
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule
+      ],
+      providers: [
+        AlertController,
+        { 
+          provide: ActivatedRoute, 
+          useValue: { 
+            queryParams: of({}),
+            snapshot: { params: {} }
+          } 
+        },
+        { provide: ServiceBDService, useValue: serviceSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EditarPerfilPage);
     component = fixture.componentInstance;
+    
+    // Inicializa 'usuario' con un objeto predeterminado
+    component.usuario = {
+      nombre: 'Angel',
+      correo: 'angel@gmail.com',
+      imagen: 'any',
+      id: 1
+    };
+    
+    alertController = TestBed.inject(AlertController);
+    serviceBDService = TestBed.inject(ServiceBDService) as jasmine.SpyObj<ServiceBDService>;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('VAlidar que el nombre sea valido', () => {
+    component.usuario.nombre = 'Ana123';
+    expect(component.validarNombre()).toBeFalse();
+    component.usuario.nombre = 'Ana';
+    expect(component.validarNombre()).toBeTrue();
+  });
+  
 });
--- automotora/src/app/pages/ver-perfil/ver-perfil.page.spec.ts
+++ automotora/src/app/pages/ver-perfil/ver-perfil.page.spec.ts
@@ -1,17 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, flush } from '@angular/core/testing';
 import { VerPerfilPage } from './ver-perfil.page';
+import { ServiceBDService } from 'src/app/service/service-bd.service';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { ChangeDetectorRef } from '@angular/core';
 
 describe('VerPerfilPage', () => {
   let component: VerPerfilPage;
   let fixture: ComponentFixture<VerPerfilPage>;
+  let bdServiceSpy: jasmine.SpyObj<ServiceBDService>;
+  let storageSpy: jasmine.SpyObj<NativeStorage>;
+  let cdrSpy: jasmine.SpyObj<ChangeDetectorRef>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const bdSpy = jasmine.createSpyObj('ServiceBDService', ['searchUserById']);
+    const storage = jasmine.createSpyObj('NativeStorage', ['getItem']);
+    const cdr = jasmine.createSpyObj('ChangeDetectorRef', ['detectChanges']);
+
+    await TestBed.configureTestingModule({
+      declarations: [VerPerfilPage],
+      providers: [
+        { provide: ServiceBDService, useValue: bdSpy },
+        { provide: NativeStorage, useValue: storage },
+        { provide: ChangeDetectorRef, useValue: cdr },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(VerPerfilPage);
     component = fixture.componentInstance;
+    bdServiceSpy = TestBed.inject(ServiceBDService) as jasmine.SpyObj<ServiceBDService>;
+    storageSpy = TestBed.inject(NativeStorage) as jasmine.SpyObj<NativeStorage>;
+    cdrSpy = TestBed.inject(ChangeDetectorRef) as jasmine.SpyObj<ChangeDetectorRef>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
