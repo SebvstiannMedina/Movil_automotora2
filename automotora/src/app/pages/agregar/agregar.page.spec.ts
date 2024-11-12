9d2022930b1fccd445e4dbe979b277e534e0628d
Angel Rengifo <angelrengifo140@gmail.com>
2024-11-12 03:20:36 -0300

    5 de 5
    mob sex

--- automotora/src/app/pages/agregar/agregar.page.spec.ts
+++ automotora/src/app/pages/agregar/agregar.page.spec.ts
@@ -1,11 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgregarPage } from './agregar.page';
+import { AlertController } from '@ionic/angular';
+import { ServiceBDService } from 'src/app/service/service-bd.service';
+import { Router } from '@angular/router';
 
 describe('AgregarPage', () => {
   let component: AgregarPage;
   let fixture: ComponentFixture<AgregarPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AgregarPage],
+      providers: [
+        { provide: AlertController, useValue: { create: () => Promise.resolve({ present: () => Promise.resolve() }) } },
+        { provide: ServiceBDService, useValue: { insertarCrud: () => Promise.resolve() } },
+        { provide: Router, useValue: { navigate: () => Promise.resolve() } }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AgregarPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +25,11 @@ describe('AgregarPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Verifica el precio correcto', () => {
+    const event = { target: { value: '1500.50' } };
+    component.validarPrecio(event);
+    expect(component.mensajeError).toBe('');
   });
 });
