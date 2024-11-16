5a0c12a2b8fc415f791ceb67f43ce7f64e1c9015
Angel Rengifo <angelrengifo140@gmail.com>
2024-11-16 02:26:34 -0300

    mas

--- automotora/src/app/pages/editar/editar.page.spec.ts
+++ automotora/src/app/pages/editar/editar.page.spec.ts
@@ -1,11 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditarPage } from './editar.page';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
+import { ServiceBDService } from 'src/app/service/service-bd.service';
+import { Camera } from '@capacitor/camera';
+import { of } from 'rxjs';
+
+// Mock del servicio ServiceBDService
+class MockServiceBDService {
+  dbState() { return of(true); }
+  fetchCategoria() { return of([{ id: 1, nombre: 'Categoria 1' }]); }
+  modificarCrud() { return of(null); }
+}
 
 describe('EditarPage', () => {
   let component: EditarPage;
   let fixture: ComponentFixture<EditarPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditarPage ],
+      imports: [IonicModule.forRoot(), RouterTestingModule],
+      providers: [
+        { provide: ServiceBDService, useClass: MockServiceBDService },
+        Camera
+      ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(EditarPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- automotora/src/app/service/service-bd.service.spec.ts
+++ automotora/src/app/service/service-bd.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { AlertController, Platform } from '@ionic/angular';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { Router } from '@angular/router';
 import { ServiceBDService } from './service-bd.service';
 
 describe('ServiceBDService', () => {
   let service: ServiceBDService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        SQLite,
+        AlertController,
+        Platform,
+        NativeStorage,
+        Router
+      ]
+    });
     service = TestBed.inject(ServiceBDService);
   });
 
