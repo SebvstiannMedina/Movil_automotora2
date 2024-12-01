5b5218b9500cadac6c1a05e4591f16bfee606d6d
Angel Rengifo <angelrengifo140@gmail.com>
2024-12-01 20:29:17 -0300

    cambios
    j

--- automotora/src/app/pages/eliminar/eliminar.page.ts
+++ automotora/src/app/pages/eliminar/eliminar.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { ServiceBDService } from 'src/app/service/service-bd.service';
@@ -22,24 +22,12 @@ export class EliminarPage implements OnInit {
     }
   ]
 
-/*
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'AGREGAR ',
-      message: 'Producto fue agregado',
-      buttons: ['ok'],
-    });
-
-    await alert.present();
-    
-  }
-*/
 
   constructor(private alertController: AlertController, private bd: ServiceBDService, private router:Router) {
     this.bd.dbState().subscribe(data=>{
-      //validar si la bd esta lista
+   
       if(data){
-        //subscribir al observable de la listaNoticias
+
         this.bd.fetchCrud().subscribe(res=>{
           this.arreglocrud = res;
         })
@@ -50,9 +38,9 @@ export class EliminarPage implements OnInit {
 
   ngOnInit() {
     this.bd.dbState().subscribe(data=>{
-      //validar si la bd esta lista
+
       if(data){
-        //subscribir al observable de la listaNoticias
+
         this.bd.fetchCrud().subscribe(res=>{
           this.arreglocrud = res;
         })
@@ -67,7 +55,7 @@ export class EliminarPage implements OnInit {
       }
     }
     this.router.navigate(['/editar'], navigationsExtras);
-   // this.bd.presentAlert("hola",x.idCrud);
+
 
   }
   eliminar(x:any){
@@ -78,4 +66,90 @@ export class EliminarPage implements OnInit {
     this.router.navigate(['/agregar']);
   }
 
+}
+  */
+
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ServiceBDService } from 'src/app/service/service-bd.service';
+
+@Component({
+  selector: 'app-eliminar',
+  templateUrl: './eliminar.page.html',
+  styleUrls: ['./eliminar.page.scss'],
+})
+export class EliminarPage implements OnInit {
+  categorias: any[] = [];
+  Crud: any[] = [];
+  categoriaSeleccionada: number | null = null;
+  nombreCategoriaSeleccionada: string = '';
+
+  constructor(private alertController: AlertController, private bd: ServiceBDService, private router: Router) { }
+
+  ngOnInit() {
+    this.bd.dbState().subscribe(ready => {
+      if (ready) {
+        this.cargarCategorias();
+      }
+    });
+  }
+
+  cargarCategorias() {
+    this.bd.fetchCategoria().subscribe(categorias => {
+      this.categorias = categorias;
+    });
+  }
+
+  cargarCrudPorCategoria(idCategoria: number) {
+    this.categoriaSeleccionada = idCategoria;
+    this.nombreCategoriaSeleccionada = this.categorias.find(
+      cat => cat.idCategoria === idCategoria
+    )?.nomCateg || '';
+    
+    this.bd.fetchCrud().subscribe(Crud => {
+      this.Crud = Crud.filter(Crud => 
+        Crud.idCategoria === idCategoria
+      );
+    });
+  }
+
+  volverACategorias() {
+    this.categoriaSeleccionada = null;
+    this.Crud = [];
+  }
+
+  //////////////////////////
+
+  modificar(Crud:any){
+    let navigationsExtras: NavigationExtras = {
+      state: {
+        crud: Crud
+      }
+    }
+    this.router.navigate(['/editar'], navigationsExtras);
+
+
+  }
+
+  agregar() {
+    this.router.navigate(['/agregar']);
+  }
+
+  // Método para eliminar un Crud
+  eliminar(idcrud:any){
+    this.bd.eliminarCrud(idcrud.idCrud);
+  }
+  
+
+  // Método para mostrar alertas
+  async presentAlert(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
